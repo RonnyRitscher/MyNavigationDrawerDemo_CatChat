930794f2f38bb7b14a878f83b09ac52e211ac4ce
RonnyRitscher <r.k.ritscher@web.de>
2019-03-05 08:35:32 +0100

    2019.03.05_08:26

--- app/src/main/java/de/proneucon/mynavigationdrawerdemo_catchat/MainActivity.java
+++ app/src/main/java/de/proneucon/mynavigationdrawerdemo_catchat/MainActivity.java
@@ -1,7 +1,10 @@
 package de.proneucon.mynavigationdrawerdemo_catchat;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        //TRANSACTIONS
+        //Zum start soll das inboxFragment angezeigt werden:
+        Fragment fragment = new InboxFragment();
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        //hinzufügen des Fragments
+        transaction.add(R.id.content_frame , fragment);
+        transaction.commit();
+
     }
 }
